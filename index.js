cf5e91f51bfe874c286e10a9fb0b94b1f0e96c43
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-20 15:25:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function getRepositories(){
-  let input = document.getElementById('username').value;
-  const req = new XMLHttpResquest();
-  req.addEventListener('load', displayRepositories);
-  req.open('GET', `https://api.github.com/users/${username}/repos`);
-  req.send();
-}
+// function getRepositories(){
+//   let input = document.getElementById('username').value;
+//   const req = new XMLHttpResquest();
+//   req.addEventListener('load', displayRepositories);
+//   req.open('GET', `https://api.github.com/users/${username}/repos`);
+//   req.send();
+// }
 
 function displayCommits(){
   let commits= JSON.parse(this.responseText);
