29cb47d1908eeb1c2b49da46805190ce803bd2cf
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-20 14:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function displayCommits(){
 function displayRepositories(){
   
 }
+
+
+const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
