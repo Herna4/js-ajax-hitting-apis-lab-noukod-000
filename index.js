0fb11b197bdbe338a4ddbb8b956b1b836ed58490
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-20 14:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getRepositories(){
   let input = document.getElementById('username').value;
-  const req = XMLHttpResquest();
+  const req = new XMLHttpResquest();
   req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
