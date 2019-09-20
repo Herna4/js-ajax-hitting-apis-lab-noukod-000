71f6227432ca42d11aaf965f06039afa211014d2
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-20 15:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,3 +71,16 @@ function displayRepositories() {
   }).join('') + "</ul>";
   document.getElementById("repositories").innerHTML = repoList
 }
+
+
+
+
+
+
+// function getRepositories(){
+//   let input = document.getElementById('username').value;
+//   const req = new XMLHttpResquest();
+//   req.addEventListener('load', displayRepositories);
+//   req.open('GET', `https://api.github.com/users/${username}/repos`);
+//   req.send();
+// }
